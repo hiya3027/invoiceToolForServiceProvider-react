10f34cf9fdbd2964be5a832a47be90137e237422
Hiya Lakhani <hiyalakhani30@gmail.com>
2025-03-04 12:09:41 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
